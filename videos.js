ae3920c59830fe3590ccd1862e4d7a810d7c9e97
George Kaveladze <geokavel@Georges-MacBook-Pro.local>
2018-04-09 10:09:50 -0700

    First commit

---
+++ videos.js
@@ -0,0 +1,17 @@
+var videos = [
+{videoId:"iZNBDBRHdwc",startSeconds:1,endSeconds:24,id:"mexico1"},
+{videoId:"AneeacsvNwU",startSeconds:0,endSeconds:999,id:"mexico2"},
+{videoId:"TM6qRl3dSME",startSeconds:0,endSeconds:42,id:"newyork"},
+{videoId:"4f7HYhfazCM",startSeconds:41,endSeconds:999,id:"media"},
+{videoId:"wk_f_U_n9Ow",startSeconds:0,endSeconds:999,id:"sweat1"},
+{videoId:"Sclb5H6f8dE",startSeconds:0,endSeconds:999,id:"sweat2"},
+{videoId:"VipZ54eZb1g",startSeconds:0,endSeconds:999,id:"little1"},
+{videoId:"gpNL-WB3D0E",startSeconds:0,endSeconds:999,id:"little2"},
+{videoId:"7p_ltuXcOH4",startSeconds:0,endSeconds:999,id:"little3"},
+{videoId:"F8a4krfAJbI",startSeconds:20,endSeconds:32,id:"cruz1"},
+{videoId:"mp1gSBgMZAA",startSeconds:0,endSeconds:999,id:"cruz2"},
+{videoId:"zO7UL5dVGfQ",startSeconds:0,endSeconds:999,id:"kelly1"},
+{videoId:"j2bFYQZcAwU",startSeconds:0,endSeconds:999,id:"kelly2"},
+{videoId:"z_u4rq_03Y0",startSeconds:0,endSeconds:999,id:"hillary1"},
+{videoId:"5r92qx9p9wM",startSeconds:0,endSeconds:999,id:"hillary2"}
+];
