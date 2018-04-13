9a2f878dc646122d2a5950baeb0d417035533e2a
George Kaveladze <geokavel@Georges-MacBook-Pro.local>
2018-04-12 23:25:34 -0700

    Set up XML file creation

--- record.php
+++ record.php
@@ -1,11 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin:http://rawgit.com");
 $times = array();
 $videos = array();
 $mc = array();
 $reasons = array();
-foreach($_POST as $key=>$val) {
-$key = $test_input($key);
-$val = $test_input($val);
+foreach($_GET as $key=>$val) {
+$key = test_input($key);
+$val = test_input($val);
 if(strpos($key,"reason")===0) {
 $reasons[] = $val;
 }
@@ -19,10 +20,39 @@ else if($key=="mc") {
 $mc = str_getcsv($val);
 }
 }
-echo join($reasons);
+
+$xml=<<<XML
+<data>
+</data>
+XML;
+$data = new SimpleXMLElement($xml);
+date_default_timezone_set("America/Los_Angeles");
+$data->addChild("date",date("m/d/y g:i:s a"));
+$whistles = $data->addChild("whistles");
+$quiz = $data->addChild("mc");
+
 for($i=0;$i<count($reasons);$i++) {
-	echo "<div>".$reasons[$i]." ".$times[$i]." ".$videos[$i]."</div>";
+	$w = $whistles->addChild("whistle",$reasons[$i]);
+	$w->addAttribute("video",$videos[$i]);
+	$w->addAttribute("time",$times[$i]);
 }
+
+foreach($mc as $choice) {
+	$quiz->addChild("answer",$choice);
+}
+
+$dir = "data";
+if(!file_exists($dir)) {
+	mkdir($dir);
+}
+$name="person";
+$file = "";
+for($num = 1;file_exists(($file = $dir."/".$name.$num.".xml"));$num++) {}
+echo $file;
+ $data->asXML($file) or die("Sad!");
+
+echo $data->asXML();
+
 
 
 function test_input($data) {
